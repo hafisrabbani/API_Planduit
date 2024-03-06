68f31db53cc542911c31642e808120a7e3d294d7
hafisrabbani <hafis.gov@gmail.com>
2024-03-07 02:24:22 +0700

    adding cache for optimizing speed

--- app/Services/Hook/HookGoldAPI/HookGoldAPIServiceImplement.php
+++ app/Services/Hook/HookGoldAPI/HookGoldAPIServiceImplement.php
@@ -4,22 +4,23 @@ namespace App\Services\Hook\HookGoldAPI;
 
 use GuzzleHttp\Client;
 use LaravelEasyRepository\Service;
-
+use Illuminate\Support\Facades\Cache;
 class HookGoldAPIServiceImplement extends Service implements HookGoldAPIService
 {
-
-    /**
-     * don't change $this->mainRepository variable name
-     * because used in extends service class
-     */
     const API_URL = 'https://logam-mulia-api.vercel.app/prices/hargaemas-org';
 
     public function getGoldPrice(): int
     {
         try {
+            $cacheKey = 'gold_price';
+            $cacheTime = now()->addHours(6);
+            if (Cache::has($cacheKey)) {
+                return Cache::get($cacheKey);
+            }
             $client = new Client();
             $response = $client->get(self::API_URL);
             $response = json_decode($response->getBody(), true);
+            Cache::put($cacheKey, $response['data'][0]['buy'], $cacheTime);
             return $response['data'][0]['buy'];
         }catch (\Exception $e) {
             throw new \Exception($e->getMessage());
--- app/Services/Hook/HookGrainAPI/HookGrainAPIServiceImplement.php
+++ app/Services/Hook/HookGrainAPI/HookGrainAPIServiceImplement.php
@@ -4,16 +4,22 @@ namespace App\Services\Hook\HookGrainAPI;
 
 use GuzzleHttp\Client;
 use LaravelEasyRepository\Service;
-
+use Illuminate\Support\Facades\Cache;
 class HookGrainAPIServiceImplement extends Service implements HookGrainAPIService{
     private $grainAPI = "https://panelharga.badanpangan.go.id/data/harga-provinsi/";
     public function getGrainPrice(): int
     {
         try {
+            $cacheKey = 'grain_price';
+            $cacheTime = now()->addHours(6);
+            if (Cache::has($cacheKey)) {
+                return Cache::get($cacheKey);
+            }
             $client = new Client();
             $this->setTimeApi();
             $response = $client->get($this->grainAPI);
             $response = json_decode($response->getBody(), true);
+            Cache::put($cacheKey, $response['hargaratarata'], $cacheTime);
             return $response['hargaratarata'];
         }catch (\Exception $e) {
             throw new \Exception($e->getMessage());
