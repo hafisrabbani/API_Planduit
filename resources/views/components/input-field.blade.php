7e1029c13f807165cbd04b28955cd4c19e120b1d
hafisrabbani <hafis.gov@gmail.com>
2024-03-18 17:48:44 +0700

    fixing bug

--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -1,4 +1,4 @@
 <div>
     <label for="{{ $name }}" class="form-label">{{ $label }}</label>
-    <input type="{{ $type }}" class="form-control" name="{{ $name }}" value="{{ $value ?? '' }}">
+    <input type="{{ $type }}" class="form-control" name="{{ $name }}" id="{{ $id ?? '' }}" value="{{ $value ?? '' }}">
 </div>
